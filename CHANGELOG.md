726198e57d699dca42795edd01f8389689d43e66
hyunwoo.park <sweat0713@gmail.com>
2026-08-12 16:57:09 +0900

    spring boot project 환경 구성

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## CHANGELOG
+
+#### 1. 프로젝트 환경 구성
+
+✅ Spring Boot 환경 구성
+
---
+++ src/main/java/com/fc/fcauth/FcAuthApplication.java
@@ -0,0 +1,13 @@
+package com.fc.fcauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcAuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FcAuthApplication.class, args);
+  }
+
+}
