5d44087137be777b2557ac4adb8acdd09dbb31cd
hyunwoo.park <sweat0713@gmail.com>
2026-08-20 16:46:02 +0900

    확인된 정보가 등록된 유저의 정보인지 판단

--- src/main/java/com/fc/fcauth/controller/KakaoLoginController.java
+++ src/main/java/com/fc/fcauth/controller/KakaoLoginController.java
@@ -1,7 +1,9 @@
 package com.fc.fcauth.controller;
 
 import com.fc.fcauth.model.KakaoUserInfoRespDto;
+import com.fc.fcauth.repository.EmployeeRepository;
 import com.fc.fcauth.service.KakaoService;
+import com.fc.fcauth.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -14,15 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class KakaoLoginController {
-  
-  private final KakaoService kakaoService;
-  
+
+  private final LoginService loginService;
+
   @GetMapping("/kakao/callback")
   public ResponseEntity callback(@RequestParam("code") String code) {
-    String token = kakaoService.getAccessTokenFromKakao(code);
-    KakaoUserInfoRespDto dto = kakaoService.getUserFromKakao(token);
-    log.info("nickname : {}", dto.getKakaoAccount().getProfile().getNickname());
-    return new ResponseEntity(HttpStatus.OK);
+    return loginService.login(code);
+
   }
 
 }
---
+++ src/main/java/com/fc/fcauth/service/LoginService.java
@@ -0,0 +1,31 @@
+package com.fc.fcauth.service;
+
+import com.fc.fcauth.model.KakaoUserInfoRespDto;
+import com.fc.fcauth.repository.EmployeeRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+  private final KakaoService kakaoService;
+  private final EmployeeRepository employeeRepository;
+
+
+  public ResponseEntity login(String code) {
+    String token = kakaoService.getAccessTokenFromKakao(code);
+    KakaoUserInfoRespDto dto = kakaoService.getUserFromKakao(token);
+    String nickName = dto.getKakaoAccount().getProfile().getNickname();
+
+    if(employeeRepository.existsByKakaoNickName(nickName)){
+      return new ResponseEntity("환영합니다 " + nickName, HttpStatus.OK);
+    }
+    else {
+      return new ResponseEntity("등록된 임직원이 아닙니다.", HttpStatus.FORBIDDEN);
+    }
+  }
+}
